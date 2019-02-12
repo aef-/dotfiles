fd59e6c43c33e918ffc7d87093dcf74ccda46e53
Adrian Fraiha <afraiha@makespace.com>
2019-02-12 11:05:37 -0500

    Add pure + minor updates to config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,2 @@
 source ~/.asdf/asdf.fish
 set -gx PATH /usr/local/sbin $PATH
-eval (python -m virtualfish)
---
+++ omf/before.init.fish
@@ -0,0 +1,1 @@
+set -g VIRTUALFISH_PYTHON "/Users/adrianfraiha/.asdf/installs/python/3.7.2/bin/python"
