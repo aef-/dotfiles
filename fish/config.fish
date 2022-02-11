701bce2cbad649488ab8cb4425bbbcb23f306c29
aef- <aef@fastmail.com>
2022-02-10 22:17:24 -0500

    rest of config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set PATH $PATH /usr/lib/psql12/bin
+    set PATH $PATH /home/adrian/.local/bin
+    set PATH $PATH /home/adrian/.mix/escripts
+    set PATH $PATH /home/adrian/.cargo/env
+    set PATH $PATH /home/adrian/.cargo/bin
+end
---
+++ fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/adrian/.fzf/shell/key-bindings.fish
